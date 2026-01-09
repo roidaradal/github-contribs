0a516c73d08d21991efb5ad3b1f5ebc8711f412c
roidaradal <johnroy.daradal@goodkredit.com>
2026-01-09 15:26:54 +0800

    Dev contributions bar graph

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # Github Contributions 
 # John Roy Daradal 
 
-import requests
+import json, requests
+import matplotlib.pyplot as plt 
+import numpy as np
 from datetime import date
 from bs4 import BeautifulSoup
 
@@ -17,14 +19,14 @@ def fetch_contributions(username: str) -> dict[str,int]:
     # Start from January 1 of current year
     start = '%d-01-01' % date.today().year
     url = HTML_URL % (username, start)
-    start_date = date_to_string(START_DATE)
-    end_date = date_to_string(END_DATE)
 
     # Fetch HTML page and use BeautifulSoup
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, 'lxml')
 
     # Get contributions
+    start_date = date_to_string(START_DATE)
+    end_date = date_to_string(END_DATE)
     contribs: dict[str, int] = {}
     cells = soup.select('td.ContributionCalendar-day')
     for cell in cells:
@@ -39,19 +41,46 @@ def fetch_contributions(username: str) -> dict[str,int]:
         if tooltip: text = tooltip.get_text()
 
         count = text.strip().split()[0]
-        if count != 'No':
-            contribs[cell_date] = int(count)
+        contribs[cell_date] = 0 if count == 'No' else int(count)
 
     return contribs
 
+def fetch_dev_contributions():
+    f = open('devs.json', 'r')
+    devs: list[str] = json.load(f)
+    f.close()
+
+    count: dict[str, int] = {}
+    for username in devs:
+        contribs = fetch_contributions(username)
+        count[username] = sum(contribs.values())
+        print(username, count[username])
+
+    entries = sorted(count.items(), key=lambda x: x[1])
+    names: list[str] = [x[0] for x in entries]
+    counts: list[int] = [x[1] for x in entries]
+
+    _, ax = plt.subplots()
+    bars = plt.barh(names, counts)
+    ax.bar_label(bars, label_type='edge', padding=5)
+    plt.title('GitHub Contributions from %s to %s' % (date_to_string(START_DATE), date_to_string(END_DATE)))
+    plt.xlabel('Contribs')
+    plt.ylabel('Devs')
+
+    plt.show()
 
 def date_to_string(d: Date) -> str:
     yy, mm, dd = d 
     return '%d-%.2d-%.2d' % (yy, mm, dd)
 
 if __name__ == '__main__':
-    username = 'roidaradal'
-    contribs = fetch_contributions(username)
-    print(username, sum(contribs.values()))
-    for k, v in sorted(contribs.items()):
-        print(k, v)
+    fetch_dev_contributions()
+
+'''
+TODO:
+[ ] Detect current week (Mon-Fri) automatically
+[ ] Detect current month automatically 
+[ ] Separate into days (Mon-Fri) + weekends 
+[ ] Colored graphs similar to Github's contribs grid
+[ ] Use Github API for public events
+'''
