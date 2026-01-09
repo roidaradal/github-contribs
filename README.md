82070071e4cc775279f237da9dbcd64634c5a180
roidaradal <johnroy.daradal@goodkredit.com>
2026-01-09 14:52:12 +0800

    Extract contribution counts

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # github-contribs 
 
-Tool for pulling Github contributions count of Github users
+Python tool for pulling GitHub contribution count of GitHub users
+
+## Dependencies
+* `requests` - for making HTTP requests
+* `BeautifulSoup4` - for traversing HTML DOM
--- main.py
+++ main.py
@@ -3,16 +3,55 @@
 
 import requests
 from datetime import date
+from bs4 import BeautifulSoup
+
+Date = tuple[int,int,int]
 
 HTML_URL: str = 'https://github.com/users/%s/contributions?from=%s'
 API_URL : str = 'https://api.github.com/users/%s/events/public'
 
-def fetch_contributions(username: str):
+START_DATE: Date = (2026,1,5)
+END_DATE  : Date = (2026,1,9)
+
+def fetch_contributions(username: str) -> dict[str,int]:
+    # Start from January 1 of current year
     start = '%d-01-01' % date.today().year
     url = HTML_URL % (username, start)
+    start_date = date_to_string(START_DATE)
+    end_date = date_to_string(END_DATE)
+
+    # Fetch HTML page and use BeautifulSoup
     resp = requests.get(url)
-    print(resp.text)
+    soup = BeautifulSoup(resp.text, 'lxml')
+
+    # Get contributions
+    contribs: dict[str, int] = {}
+    cells = soup.select('td.ContributionCalendar-day')
+    for cell in cells:
+        cell_date = str(cell['data-date'])
+        if not (start_date <= cell_date <= end_date): continue # skip if not within date range
+
+        # Find the tooltip associated with td cell
+        tooltip = soup.find('tool-tip', attrs={'for': cell['id']})
+
+        # Extract the count from tooltip's inner text
+        text = 'No'
+        if tooltip: text = tooltip.get_text()
+
+        count = text.strip().split()[0]
+        if count != 'No':
+            contribs[cell_date] = int(count)
+
+    return contribs
+
+
+def date_to_string(d: Date) -> str:
+    yy, mm, dd = d 
+    return '%d-%.2d-%.2d' % (yy, mm, dd)
 
 if __name__ == '__main__':
     username = 'roidaradal'
-    fetch_contributions(username)
+    contribs = fetch_contributions(username)
+    print(username, sum(contribs.values()))
+    for k, v in sorted(contribs.items()):
+        print(k, v)
