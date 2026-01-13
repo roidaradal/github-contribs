cc2b0a526675525b16f3f362f15fba7a1f74ebf2
roidaradal <roidaradal@gmail.com>
2026-01-13 21:53:20 +0800

    Detect current week

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 Python tool for pulling GitHub contribution count of GitHub users
 
+## Usage 
+Prepare a `devs.json` file which contains the list of GitHub usernames. 
+
+Run `python main.py` to pull the users' GitHub pages, and generate a report of their contributions for the week.
+
 ## Dependencies
 * `requests` - for making HTTP requests
 * `BeautifulSoup4` - for traversing HTML DOM
--- main.py
+++ main.py
@@ -7,13 +7,11 @@ from datetime import date
 from bs4 import BeautifulSoup
 
 Date = tuple[int,int,int]
+DateRange = tuple[Date,Date]
 IntPair = tuple[int,int]
 
 HTML_URL: str = 'https://github.com/users/%s/contributions?from=%s'
 API_URL : str = 'https://api.github.com/users/%s/events/public'
-
-START_DATE: Date = (2026,1,5)
-END_DATE  : Date = (2026,1,9)
 
 class Contrib:
     def __init__(self):
@@ -23,49 +21,17 @@ class Contrib:
     def __repr__(self) -> str:
         return str(self.count)
 
-def fetch_contributions(username: str) -> dict[str, Contrib]:
-    # Start from January 1 of current year
-    start = '%d-01-01' % date.today().year
-    url = HTML_URL % (username, start)
-
-    # Fetch HTML page and use BeautifulSoup
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'lxml')
-
-    # Get contributions
-    start_date = date_to_string(START_DATE)
-    end_date = date_to_string(END_DATE)
-    contribs: dict[str, Contrib] = {}
-    cells = soup.select('td.ContributionCalendar-day')
-    for cell in cells:
-        cell_date = str(cell['data-date'])
-        if not (start_date <= cell_date <= end_date): continue # skip if not within date range
-
-        # Find the tooltip associated with td cell
-        tooltip = soup.find('tool-tip', attrs={'for': cell['id']})
-
-        # Extract the count from tooltip's inner text
-        text = 'No'
-        if tooltip: text = tooltip.get_text()
-
-        count = text.strip().split()[0]
-        contrib = Contrib()
-        contrib.count = 0 if count == 'No' else int(count)
-        contrib.level = int(str(cell['data-level']))
-        contribs[cell_date] = contrib
-
-    return contribs
-
 def fetch_dev_contributions():
+    start, end = get_current_week()
     count: dict[str, int] = {}
     levels: dict[str, list[IntPair]] = {}
     for username in get_devs():
-        contribs = fetch_contributions(username)
-        levels[username] = get_count_levels(contribs)
+        contribs = fetch_contributions(username, (start, end))
+        levels[username] = get_count_levels(contribs, (start, end))
         count[username] = sum([x.count for x in contribs.values()])
         print(username, count[username], levels[username])
 
-    title = get_title()
+    title = 'GitHub Contributions from %s to %s' % (date_to_string(start), date_to_string(end))
     entries = sorted(count.items(), key=lambda x: x[1])
     names: list[str] = [x[0] for x in entries]
     counts: list[int] = [x[1] for x in entries]
@@ -90,7 +56,49 @@ def fetch_dev_contributions():
     reps['Table'] = ''.join(tbl)
     create_grid_file(reps)
 
+def fetch_contributions(username: str, date_range: DateRange) -> dict[str, Contrib]:
+    # Start from January 1 of current year
+    year_start = '%d-01-01' % date.today().year
+    url = HTML_URL % (username, year_start)
 
+    # Fetch HTML page and use BeautifulSoup
+    resp = requests.get(url)
+    soup = BeautifulSoup(resp.text, 'lxml')
+
+    # Get contributions
+    start, end = date_range
+    start_date = date_to_string(start)
+    end_date = date_to_string(end)
+    contribs: dict[str, Contrib] = {}
+    cells = soup.select('td.ContributionCalendar-day')
+    for cell in cells:
+        cell_date = str(cell['data-date'])
+        if not (start_date <= cell_date <= end_date): continue # skip if not within date range
+
+        # Find the tooltip associated with td cell
+        tooltip = soup.find('tool-tip', attrs={'for': cell['id']})
+
+        # Extract the count from tooltip's inner text
+        text = 'No'
+        if tooltip: text = tooltip.get_text()
+
+        count = text.strip().split()[0]
+        contrib = Contrib()
+        contrib.count = 0 if count == 'No' else int(count)
+        contrib.level = int(str(cell['data-level']))
+        contribs[cell_date] = contrib
+    return contribs
+
+################### UTILITY FUNCTIONS #########################
+
+def get_current_week() -> DateRange:
+    # For now, assumes same month 
+    # TODO: fix for week that spans two months
+    today = date.today()
+    year, month, day = today.year, today.month, today.day 
+    start_day = day - today.weekday() # start at Monday
+    end_day = start_day + 4
+    return (year, month, start_day), (year, month, end_day)
 
 def date_to_string(d: Date) -> str:
     yy, mm, dd = d 
@@ -102,14 +110,15 @@ def get_devs() -> list[str]:
     f.close()
     return devs
 
-def get_count_levels(contribs: dict[str, Contrib]) -> list[IntPair]:
+def get_count_levels(contribs: dict[str, Contrib], date_range: DateRange) -> list[IntPair]:
     pairs: list[IntPair] = []
     # For now, assumes same month
     # TODO: fix date range that spans two months
 
-    year, month = START_DATE[0:2]
-    start_day = START_DATE[2]
-    last_day = END_DATE[2]
+    start, end = date_range
+    year, month = start[0:2]
+    start_day = start[2]
+    last_day = end[2]
     for day in range(start_day, last_day+1):
         key = date_to_string((year, month, day))
         count, level = 0, 0
@@ -138,21 +147,16 @@ def create_grid_file(reps: dict[str, str]):
     url = 'file://' + full_path 
     webbrowser.open_new_tab(url)
 
-
-def get_title() -> str:
-    return 'GitHub Contributions from %s to %s' % (date_to_string(START_DATE), date_to_string(END_DATE))
-
-
 if __name__ == '__main__':
     fetch_dev_contributions()
 
 '''
-TODO:
-[ ] Open output HTML automatically
-[ ] Detect current week (Mon-Fri) automatically
-[ ] Detect current month automatically 
-[ ] Separate into days (Mon-Fri) + weekends 
-[ ] Colored graphs similar to Github's contribs grid
-[ ] Use Github API for public events
+TODO
 [ ] Fix range that spans two months (e.g. last week of Jan - first week of Feb)
+    - get_count_levels
+    - get_current_week
+[ ] Detect current month automatically 
+[ ] Generate monthly report
+    - Separate into days (Mon-Fri) + weekends 
+[ ] Use Github API for public events
 '''
